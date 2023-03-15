226de1c8eaf913fe6060c10984866c009a3cc212
Liu Zexuan <liuzex@sheridancollege.ca>
2023-03-14 21:36:46 -0400

    create listview for booklist page

---
+++ BookFinders/BookFinders/Model/book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFinders.Model
+{
+     class book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
--- BookFinders/BookFinders/bookList.xaml.cs
+++ BookFinders/BookFinders/bookList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookFinders.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ namespace BookFinders
         public bookList()
         {
             InitializeComponent();
+            List<book> booklist = new List<book>
+            {
+                new book(){Name = "My book Cover",Author="Peter", Description="This is a very good book", ImageLink="bookImage.jpg" },
+                new book(){Name = "Intro to Java",Author="Roman", Description="This is a good Java book", ImageLink="bookImage.jpg" }
+            };
+            bookLists.ItemsSource = booklist;
+        }
+
+        private void bookLists_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var book = bookLists.SelectedItem as book;
+            Navigation.PushModalAsync(new bookDetails());
         }
     }
 }
