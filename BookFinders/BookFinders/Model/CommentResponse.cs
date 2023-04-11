57e50894c944e6459c7a21e4183cdfea94ad0214
Liu Zexuan <liuzex@sheridancollege.ca>
2023-04-11 18:58:55 -0400

    implement get request at client

---
+++ BookFinders/BookFinders/Model/CommentResponse.cs
@@ -0,0 +1,15 @@
+﻿using BookFindersLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFinders.Model
+{
+    public class CommentResponse
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public List<Comment> data { get; set; }
+        public object errors { get; set; }
+    }
+}
--- BookFinders/BookFinders/bookDetails.xaml.cs
+++ BookFinders/BookFinders/bookDetails.xaml.cs
@@ -1,18 +1,28 @@
 ﻿using BookFinders.Model;
+using BookFindersLibrary.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace BookFinders
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class bookDetails : ContentPage
     {
+        private List<Comment> commentsList;
+        private HttpClient client;
         public bookDetails(book bookObj)
         {
             InitializeComponent();
@@ -21,6 +31,36 @@ namespace BookFinders
             bookAuthor.Text = "Author:" + bookObj.Author;
             bookDesc.Text =  bookObj.Description;
             bookImage.Source = "bookImage.jpg";
+
+            var handler = new HttpClientHandler();
+
+            // Set the ServerCertificateCustomValidationCallback to a delegate that accepts any certificate
+            handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+
+            commentsList = new List<Comment>();
+            client = new HttpClient(handler);
+            LoadComments();
+
         }
+
+        async void LoadComments()
+        {
+            var response = await client.GetAsync("https://10.0.2.2:7042/api/Comment/getcomments");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var comments = JsonConvert.DeserializeObject<CommentResponse>(content);
+                commentsList = comments.data;
+                commentListView.ItemsSource = commentsList;
+                Debug.WriteLine("da"+comments);
+            }
+            else
+            {
+                Debug.WriteLine("it failed");
+            }
+        }
+
+
+
     }
 }
