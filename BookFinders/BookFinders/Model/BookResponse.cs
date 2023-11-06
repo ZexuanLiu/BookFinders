217f429ba51ef2f17eacbd9a6cd3c28031a79818
Liu Zexuan <liuzex@sheridancollege.ca>
2023-11-05 22:43:57 -0500

    test on Library api

---
+++ BookFinders/BookFinders/Model/BookResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFinders.Model
+{
+    class BookResponse
+    {
+        public List<Doc> docs { get; set; }
+    }
+    class Doc
+    {
+        public Pnx pnx { get; set; }
+    }
+    class Pnx
+    {
+        public PnxSort sort { get; set; }
+        public PnxControl control { get; set; }
+        public PnxSearch search { get; set; }
+    }
+    class PnxControl
+    {
+        public List<string> pqid { get; set; }
+    }
+    class PnxSort
+    {
+        public List<string> creationdate { get; set; }
+        public List<string> author { get; set; }
+        public List<string> title { get; set; }
+    }
+    class PnxSearch
+    {
+        public List<string> description { get; set; }   
+    }
+}
--- BookFinders/BookFinders/bookList.xaml.cs
+++ BookFinders/BookFinders/bookList.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using BookFinders.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,18 +21,68 @@ namespace BookFinders
     {
         private User currentUser;
         List<book> booklist;
+        private HttpClient client;
+        private ObservableCollection<book> booksList;
+
         public bookList(User user)
         {
             InitializeComponent();
-            booklist = new List<book>
-            {
-                new book(){Id = "001", Name = "My book Cover",Author="Peter", Description="This is a very good book", ImageLink="bookImage.jpg" },
-                new book(){Id = "002", Name = "Intro to Java",Author="Roman", Description="This is a good Java book", ImageLink="bookImage.jpg" }
-            };
-            bookLists.ItemsSource = booklist;
+           //booklist = new List<book>
+           // {
+           //     new book(){Id = "001", Name = "My book Cover",Author="Peter", Description="This is a very good book", ImageLink="bookImage.jpg" },
+           //     new book(){Id = "002", Name = "Intro to Java",Author="Roman", Description="This is a good Java book", ImageLink="bookImage.jpg" }
+           // };
+           // bookLists.ItemsSource = booklist;
+
             currentUser = user;
+            booksList = new ObservableCollection<book>();
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            client = new HttpClient(handler);
         }
 
+        private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            string searchText = searchBar.Text;
+            LoadBooks(searchText);
+        }
+        public async void LoadBooks(string searchText)
+        {
+            var response = await client.GetAsync("\r\nhttps://api-ca.hosted.exlibrisgroup.com/primo/v1/search?vid=01OCLS_SHER%3ASHER&tab=Everything&scope=MyInst_and_CI&q=q%3Dany%2Ccontains%2Cjavascript&lang=eng&offset=0&limit=1&sort=rank&pcAvailability=true&getMore=0&conVoc=true&inst=01OCLS_SHER&skipDelivery=true&disableSplitFacets=true&apikey=l8xxbd240191e506439380215edab4ec4d85");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var bookObjLists = JsonConvert.DeserializeObject<BookResponse>(content);
+
+                List<Doc> docs = bookObjLists.docs;
+                List<book> books = new List<book>();
+                foreach (var doc in docs)
+                {
+                    PnxSort sort = doc.pnx.sort;
+                    PnxControl control = doc.pnx.control;
+                    PnxSearch search = doc.pnx.search;
+
+                    var book = new book
+                    {
+                        Id = "1",
+                        Name = sort.title[0],
+                        Author = sort.author[0],
+                        Description = search.description[0],
+                        ImageLink = "N/a"
+
+                    };
+                    books.Add(book);
+                }
+
+                bookLists.ItemsSource = books;
+                OnAppearing();
+            }
+            else
+            {
+                Debug.WriteLine("failed to fetch the comment");
+            }
+        }
         private void bookLists_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             var bookObj = bookLists.SelectedItem as book;
@@ -35,26 +91,27 @@ namespace BookFinders
             Navigation.PushAsync(new bookDetails(bookObj,currentUser));
         }
 
-        private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
-        {
-            string searchText = e.NewTextValue;
-            List<book> filteredBooks = new List<book>();
+      //  private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
+      //  {
+      //      string searchText = e.NewTextValue;
+      //      List<book> filteredBooks = new List<book>();
 
-            if (!string.IsNullOrEmpty(searchText))
-            {
-                filteredBooks = booklist.Where(b => b.Name.Contains(searchText) || b.Author.Contains(searchText)).ToList();
-            }
-            else
-            {
-                filteredBooks = booklist;
-            }
+      //      if (!string.IsNullOrEmpty(searchText))
+      //      {
+      //          filteredBooks = booklist.Where(b => b.Name.Contains(searchText) || b.Author.Contains(searchText)).ToList();
+      //      }
+      //      else
+      //      {
+      //          filteredBooks = booklist;
+      //      }
 
-            bookLists.ItemsSource = filteredBooks;
-        }
+      //      bookLists.ItemsSource = filteredBooks;
+      //  }
 
         private async void OnBack(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
         }
+
     }
 }
