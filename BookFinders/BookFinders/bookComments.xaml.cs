82cf54b5b4cbe13d58bfdeb7ae1cd0a1e80aa775
liuzex <liuzex@sheridancollege.ca>
2023-12-10 15:48:42 -0500

    implement the post and read for comment feature.

--- BookFinders/BookFinders/bookComments.xaml.cs
+++ BookFinders/BookFinders/bookComments.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
 using BookFinders.Model;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace BookFinders
@@ -9,11 +13,37 @@ namespace BookFinders
 	{
         private Book bookObject;
         private User userObj;
+        private HttpClient client;
+        private ObservableCollection<Comment> commentsList;
+
         public bookComments (Book bookObj, User currentUser)
 		{
 			InitializeComponent ();
             bookObject = bookObj;
             userObj = currentUser;
+            var handler = new HttpClientHandler();
+
+            // Set the ServerCertificateCustomValidationCallback to a delegate that accepts any certificate
+            handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            client = new HttpClient(handler);
+            commentsList = new ObservableCollection<Comment>();
+            LoadComments(bookObject.Id);
+        }
+        public async void LoadComments(string bookId)
+        {
+            var response = await client.GetAsync("http://localhost:5156/api/Comment/getcomments/" + bookId);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var comments = JsonConvert.DeserializeObject<CommentResponse>(content);
+                commentsList = comments.data;
+                commentListView.ItemsSource = commentsList;
+                OnAppearing();
+            }
+            else
+            {
+                Debug.WriteLine("failed to fetch the comment");
+            }
         }
 
         void OnCloseButtonTapped(System.Object sender, System.EventArgs e)
@@ -33,6 +63,11 @@ namespace BookFinders
         void AddCommentsBtn_Clicked(System.Object sender, System.EventArgs e)
         {
             Navigation.PushAsync(new postComment(bookObject, userObj));
+        }
+
+        void comments_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 }
--- BookFinders/BookFinders/postComment.xaml.cs
+++ BookFinders/BookFinders/postComment.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using BookFinders.Model;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace BookFinders
@@ -9,12 +15,19 @@ namespace BookFinders
 	{
         private Book bookObject;
         private User userObj;
+        private HttpClient client;
+
         public postComment (Book bookObj, User currentUser)
 		{
 			InitializeComponent ();
             bookObject = bookObj;
             userObj = currentUser;
             commentEditor.Text = "";
+            var handler = new HttpClientHandler();
+
+            // Set the ServerCertificateCustomValidationCallback to a delegate that accepts any certificate
+            handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            client = new HttpClient(handler);
         }
 
         void OnCloseButtonTapped(System.Object sender, System.EventArgs e)
@@ -30,17 +43,58 @@ namespace BookFinders
             Navigation.PopAsync();
         }
 
-        void PostButton_Clicked(System.Object sender, System.EventArgs e)
+        async void PostButton_Clicked(System.Object sender, System.EventArgs e)
         {
             if (commentEditor.Text == "")
             {
-                DisplayAlert("Post Comment Failed", "Please enter your comment.", "OK");
+                await DisplayAlert("Post Comment Failed", "Please enter your comment.", "OK");
             }
             else
             {
+                var commentObj = new Comment()
+                {
+                    UserId = userObj.Id,
+                    BookId = bookObject.Id,
+                    UserName = userObj.Name,
+                    Description = commentEditor.Text
+
+                };
+                var result = await PostComment("http://localhost:5156/api/Comment/postcomment", commentObj);
+                if (result != null)
+                {
+                    await DisplayAlert("Post Comment Success", "You comment has been posted.", "OK");
+                    var previousPage = Navigation.NavigationStack[Navigation.NavigationStack.Count - 2] as bookComments;
+                    if (previousPage != null)
+                    {
+                       previousPage.LoadComments(bookObject.Id);
+                    }
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    await DisplayAlert("Post Comment Failed", "Please check connection.", "OK");
+                }
 
             }
         }
+
+        public async Task<string> PostComment(string url, Comment comment)
+        {
+            var json = JsonConvert.SerializeObject(comment);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine("success");
+                return responseContent;
+            }
+
+            return null;
+        }
+
+
     }
 }
 
