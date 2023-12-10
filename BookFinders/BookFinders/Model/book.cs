b3921d4b92185b0a1012abe4db03b3f4be009e62
liuzex <liuzex@sheridancollege.ca>
2023-12-10 03:21:52 -0500

    move project to IOS

--- BookFinders/BookFinders/Model/book.cs
+++ BookFinders/BookFinders/Model/book.cs
@@ -4,12 +4,18 @@ using System.Text;
 
 namespace BookFinders.Model
 {
-     public class book
+     public class Book
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
         public string ImageLink { get; set; }
+        public string LocationCode { get; set; }
+        public string LibraryCode { get; set; }
+        public string LocationBookShelfNum { get; set; }
+        public string LocationBookShelfSide { get; set; }
+        public int LocationBookShelfRow { get; set; }
+        public int LocationBookShelfColumn { get; set; }
     }
 }
