172577464bf483fa0a5da6f76bd89f33e8d01960
andreeapo <andreeapopa2000@gmail.com>
2024-10-11 21:49:50 -0400

    Added simple functionality for web app

---
+++ BookFindersVirtualLibrary/Assets/Scripts/Backend/PushNotifications.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushNotifications : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField txtPushNotification;
+    [SerializeField] private Button btnSend;
+    [SerializeField] private TMP_Text notification;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnSend.onClick.AddListener(ShowNotification);
+    }
+
+    void ShowNotification()
+    {
+        var input = txtPushNotification.text;
+        notification.text = input;
+    }
+    
+    
+}
